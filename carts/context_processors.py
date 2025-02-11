b0d7c6439e1c0ee9ecfc45994db887a9f65d0903
Bijoy <bijocode@gmail.com>
2025-02-12 01:15:04 +0530

    Checkout and User item grouping

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,5 +1,4 @@
-
-from .models import Cart,CartItem
+from .models import Cart, CartItem
 from .views import _cart_id
 
 
@@ -9,11 +8,13 @@ def counter(request):
         return {}
     else:
         try:
-            cart=Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items=CartItem.objects.all().filter(cart=cart[:1])
+            cart = Cart.objects.filter(cart_id=_cart_id(request))  # Filter the cart
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.filter(user=request.user)  # For authenticated users
+            else:
+                cart_items = CartItem.objects.filter(cart=cart[:1])  # For non-authenticated users
             for cart_item in cart_items:
-                cart_count += cart_item.quantity
+                cart_count += cart_item.quantity  # Add quantity to the cart count
         except Cart.DoesNotExist:
             cart_count = 0
     return dict(cart_count=cart_count)
-           
