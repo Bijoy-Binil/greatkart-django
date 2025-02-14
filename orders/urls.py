c61d662dc128198d3259597ce8b7c4d2365fd960
Bijoy <bijocode@gmail.com>
2025-02-15 01:43:48 +0530

    product order and payments

--- orders/urls.py
+++ orders/urls.py
@@ -14,22 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home,name='home'),
-    path('store/', include('store.urls')),
-    path('cart/', include('carts.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('orders/', include('orders.urls')),
+    path('place_order/', views.place_order,name='place_order'),
+    path('payments/', views.payments,name='payments'),
+    path('order_complete/', views.order_complete,name='order_complete'),
+    
+   
+   
     
     
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
